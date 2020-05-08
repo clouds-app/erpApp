a7e03b9e67ef2c26e49dc9dee9c3a0fc0ccada56
hdeqi <hdeqi@qq.com>
2020-05-08 16:11:02 +0800

    update source

--- unpackage/dist/dev/app-plus/pages/cardBoard/inStorage/inStorage.js
+++ unpackage/dist/dev/app-plus/pages/cardBoard/inStorage/inStorage.js
@@ -1,2 +1,2 @@
-setCssToHead([".",[1],"orderText { font-size: 12px; }\n.",[1],"margin-text-center{ text-align: center; margin: ",[0,20],"; }\n.",[1],"border-top{ border-top: 1px solid #eee; }\n.",[1],"cu-form-group .",[1],"title { min-width: calc(4em + 15px); }\n",],undefined,{path:"./pages/cardBoard/inStorage/inStorage.wxss"})();
+setCssToHead([".",[1],"orderText { font-size: 12px; }\n.",[1],"margin-text-center { text-align: center; margin: ",[0,20],"; }\n.",[1],"border-top { border-top: 1px solid #eee; }\n.",[1],"cu-form-group .",[1],"title { min-width: calc(4em + 15px); }\n",],undefined,{path:"./pages/cardBoard/inStorage/inStorage.wxss"})();
 document.dispatchEvent(new CustomEvent("generateFuncReady", { detail: { generateFunc: $gwx('./pages/cardBoard/inStorage/inStorage.wxml') } }));
